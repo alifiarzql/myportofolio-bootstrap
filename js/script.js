bcbdbe956a8fc421722cb3f1704e1cc6a458fee9
alifiarzql <rizqulalifia@gmail.com>
2025-03-26 22:03:39 +0700

    commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// even pada saat link di klik
+$('.page-scroll').on('click', function(e){
+
+    //ambil isi href
+    var tujuan = $(this).attr('href');
+    //tangkap elemen yang bersangkutan
+    var elemenTujuan = $(tujuan);
+
+    //pindahkan scroll
+    $('html, body').animate({
+        scrollTop: elemenTujuan.offset().top - 50
+    }, 1250, 'swing');
+
+    e.preventDefault;
+});
